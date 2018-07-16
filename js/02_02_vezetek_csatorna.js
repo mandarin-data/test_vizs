ba6ce31b943853feb6e0176383f5f257c8b2531d
norbertdalecker <norbertdalecker@gmail.com>
2018-07-16 15:24:50 +0200

    change width getter

--- js/02_02_vezetek_csatorna.js
+++ js/02_02_vezetek_csatorna.js
@@ -1,6 +1,6 @@
-var svg = d3.select("svg"),
+var svg = d3.select("#vis-2"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
+    width = +svg.node().getBoundingClientRect().width - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
