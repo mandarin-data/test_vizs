d64da1556d5288d1177ce5097d05aa7dae034d56
norbertdalecker <norbertdalecker@gmail.com>
2018-07-23 08:04:52 +0200

    embed barchar vis.

--- js/02_01_barchart.js
+++ js/02_01_barchart.js
@@ -5,7 +5,7 @@ function BarChart() {
 	function chart(selection) {
     	selection.each(function (d, i) {
 			
-    d3.select("svg").remove();
+    d3.select("#vis-11").remove();
 	var margin = {top: 20, right: 20, bottom: 30, left: 40},
 	width = 960 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
@@ -28,9 +28,10 @@ function BarChart() {
 		.ticks(null, "%");
 
 
-	var svg = d3.select("body").append("svg")
+	var svg = d3.select("#viscontainer-11").append("svg")
 		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("id","vis-11")
 		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
