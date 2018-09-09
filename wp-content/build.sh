b85405fc3245b980b592b4d33a61e62b897eb4ec
zolizoli <zoltan.varju@gmail.com>
2018-09-09 18:46:44 +0200

    delete wp folder, start branch

--- wp-content/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./divi/build-divi.sh
-./standalone/build-standalone.sh
-
--- wp-content/divi/build-divi.sh
+++
@@ -1,9 +0,0 @@
-for d in $(find . -type f -name "*.html"); do echo $d ; done
-for d in $(find . -type f -name "*.html"); do sed -i 's#../js#/wp-habitat/js' $d ; done
-for d in $(find . -type f -name "*.html"); do sed -i 's#../js#/wp-habitat/js#g' $d ; done
-for d in $(find . -type f -name "*.html"); do sed -i 's#../vizs#/wp-habitat/vizs#g' $d ; done
-for d in $(find . -type f -name "*.html"); do sed -i 's#../../data#/wp-habitat/data#g' $d ; done
-for d in $(find . -type f -name "*.js"); do sed -i 's#../vizs#/wp-habitat/vizs#g' $d ; done
-for d in $(find . -type f -name "*.js"); do sed -i 's#../data#/wp-habitat/data#g' $d ; done
-for d in $(find . -type f -name "*.html"); do sed -i 's#../css#/wp-habitat/css#g' $d ; done
-for d in $(find . -type f -name "*.html"); do sed -i 's#<iframe src="#<iframe src="/wp-habitat/#g' $d ; done
