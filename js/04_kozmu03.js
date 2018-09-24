5474cc7a777fba3c5d9668468f7efda36dded318
sdfghjklbn <sdfghjklbn.freemail.hu>
2018-09-24 22:51:00 +0200

    add fin vis

--- js/04_kozmu03.js
+++ js/04_kozmu03.js
@@ -1,11 +1,22 @@
+if (d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width  > 610)
+{ 
+   if (d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width  < 820){ 
+        var width_abs_kozmu03 = d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width ;
+        } else {
+         var width_abs_kozmu03 = 820    
+        }
+} else {
+  var width_abs_kozmu03 = 610 // 
+};
 
-var margin_kozmu03 = {top: 40, right: 0, bottom: 80, left: 60},
-	width = d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width - margin_kozmu03.left - margin_kozmu03.right,
-	height = 450 - margin_kozmu03.top - margin_kozmu03.bottom;
+
+var margin_kozmu03 = {top: width_abs_kozmu03/(820/40), right: 0, bottom: width_abs_kozmu03/(820/80), left: width_abs_kozmu03/(820/60)},
+	width = width_abs_kozmu03 - margin_kozmu03.left - margin_kozmu03.right,
+	height_kozmu03 = 450 - margin_kozmu03.top - margin_kozmu03.bottom;
 
 var x_kozmu03 = d3.scaleBand()
-.rangeRound([0, width - 150])
-.paddingInner(0.15)
+.rangeRound([0, width - width_abs_kozmu03/(820/150)])
+.paddingInner(0.10)
 .align(0.8);
 
 var y_kozmu03 = d3.scaleLinear()
@@ -59,13 +70,15 @@ d3.csv("../../data/04_eladosodas/04_kozmu03.csv", function(d, i, columns) {
 
 svg_kozmu03.append('text')
 		.attr('id', 'title_0304')
-		.attr("x",  (width - 150 )/2)
-		.attr("y", -25)
+		.attr("x",  (width - width_abs_kozmu03/(820/150) )/2)
+        .attr("font-size", width_abs_kozmu03/(820/17))
+		.attr("y", -width_abs_kozmu03/(820/25))
 		.text("90 napon túli késedelmes hitelszerződések száma (2017. december)");
 		
 
 	svg_kozmu03.append("g")
 		.attr("class", "axis_kozmu03")
+        .attr("font-size", width_abs_kozmu03/(820/13))    
 		.attr("transform", "translate(0," + 380 + ")")
 		.call(d3.axisBottom(x_kozmu03))
 	    .selectAll(".tick text")
@@ -77,10 +90,15 @@ svg_kozmu03.append('text')
 		.call(d3.axisLeft(y_kozmu03))
 		.append("text")
 		.attr("x", 2)
+        .attr("font-size", width_abs_kozmu03/(820/13))        
 		.attr("y", y_kozmu03(y_kozmu03.ticks().pop()))
 		.attr("dy", "0.32em")
-		.attr("text-anchor", "start");
+        .attr("text-anchor", "start");
 
+    
+    svg_kozmu03.selectAll(".tick")
+        .attr("font-size", width_abs_kozmu03/(820/13))        
+    
 	var legend_kozmu03 = svg_kozmu03.append("g")
 		.attr("text-anchor", "end")
 		.selectAll("g")
@@ -89,15 +107,16 @@ svg_kozmu03.append('text')
 		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
 	legend_kozmu03.append("rect")
-		.attr("x", width - 90)
-		.attr("width", 18)
-		.attr("height", 18)
+		.attr("x", width - width_abs_kozmu03/(820/90))
+		.attr("width", width_abs_kozmu03/(820/18))
+		.attr("height", width_abs_kozmu03/(820/18))
 		.attr("fill", z_kozmu03);
 
 	legend_kozmu03.append("text")
 		.attr("class", "legend_kozmu03")
-		.attr("x", width-60)
+		.attr("x", width-width_abs_kozmu03/(820/60))
 		.attr("y", 8)
+        .attr("font-size", width_abs_kozmu03/(820/14))    
 		.attr("dy", "0.32em")
 		.attr("text-anchor", "start")
 		.text(function(d) { return d; });
@@ -107,22 +126,23 @@ svg_kozmu03.append('text')
 		.style("visibility", "hidden");
 
 	tooltip_kozmu03.append("rect")
-		.attr("width", 60)
-		.attr("height", 20)
+		.attr("width", width_abs_kozmu03/(820/60))
+		.attr("height",width_abs_kozmu03/(820/20))
 		.attr("fill", "white")
 		.attr("stroke", "#666")
 		.attr("stroke-width", "0.5px");
 
 	tooltip_kozmu03.append("text")
-		.attr("x", 30)
+		.attr("x", width_abs_kozmu03/(820/30))
 		.attr("dy", "1.2em")
-		.style("text-anchor", "middle");	
+        .attr("font-size", width_abs_kozmu03/(820/13))
+        .style("text-anchor", "middle");	
 
 	serie_kozmu03.selectAll("rect")
 		.on("mouseout", function() { tooltip_kozmu03.style("display", "none"); })
 		.on("mousemove", function(d) {
 		var delta_kozmu03 = d[1]-d[0];
-		var xPosition_kozmu03 = d3.mouse(this)[0] - 40;
+		var xPosition_kozmu03 = d3.mouse(this)[0] - 25;
 		var yPosition_kozmu03 = d3.mouse(this)[1] - 30;
 		tooltip_kozmu03
 			.style("visibility", "visible")
@@ -133,8 +153,9 @@ svg_kozmu03.append('text')
 
 serie_kozmu03.append('text')
 	.attr("class", "kozmu03_forras")
-		.attr("x", width- 150)
-		.attr("y", 450)
+		.attr("x", width- width_abs_kozmu03/(820/150))
+		.attr("y", height_kozmu03 + margin_kozmu03.top + margin_kozmu03.bottom*0.8)
+        .attr("font-size", width_abs_kozmu03/(820/13))    
 	.attr("text-anchor", "end")  
 	.text("Adatok forrása: MNB")
 	.on('click', function(d) {
