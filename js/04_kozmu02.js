78623e30dd7e4cfa45385d9837efd17bff9f32f5
sdfghjklbn <sdfghjklbn.freemail.hu>
2018-09-27 19:07:36 +0200

    final modif

--- js/04_kozmu02.js
+++ js/04_kozmu02.js
@@ -1,205 +1,355 @@
-if (d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width  > 610)
+if (d3.select("#viscontainer-kozmu02").node().getBoundingClientRect().width  > 550)
 { 
-   if (d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width  < 820){ 
-        var width_abs_kozmu03 = d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width ;
+   if (d3.select("#viscontainer-kozmu02").node().getBoundingClientRect().width  < 900){ 
+        var width_abs_kozmu02 = d3.select("#viscontainer-kozmu02").node().getBoundingClientRect().width ;
         } else {
-         var width_abs_kozmu03 = 820    
+         var width_abs_kozmu02 = 900    
         }
 } else {
-  var width_abs_kozmu03 = 610 // 
+  var width_abs_kozmu02 = 550 // 
 };
 
 
-var margin_kozmu03 = {top: width_abs_kozmu03/(820/40), right: 0, bottom: width_abs_kozmu03/(820/100), left: width_abs_kozmu03/(820/70)},
-	width = width_abs_kozmu03 - margin_kozmu03.left - margin_kozmu03.right,
-	height_kozmu03 = 450 - margin_kozmu03.top - margin_kozmu03.bottom;
+function MyTitle(){
+    d3.selectAll("#svg_kozmu02_title > *").remove();
 
-var x_kozmu03 = d3.scaleBand()
-.rangeRound([0, width - width_abs_kozmu03/(820/100)])
-.paddingInner(0.10)
-.align(0.8);
+	var margin_kozmu02 = {top: width_abs_kozmu02/(900/20), right: 0, bottom: width_abs_kozmu02/(900/60), left: width_abs_kozmu02/(900/60)},
+		width = width_abs_kozmu02 - margin_kozmu02.left - margin_kozmu02.right,
+		height = 450 - margin_kozmu02.top - margin_kozmu02.bottom;
 
-var y_kozmu03 = d3.scaleLinear()
-.rangeRound([380, 0])
+	var svg_kozmu02_title = d3.select("#svg_kozmu02_title")
+	.attr("width", width + margin_kozmu02.left + margin_kozmu02.right)
+	.attr("height", width_abs_kozmu02/(900/50));
 
-;
+	svg_kozmu02_title.append('text')
+		.attr('id', 'kozmu02_title')
+		.attr("x",  (width -   margin_kozmu02.left - margin_kozmu02.right)/2 - margin_kozmu02.left   )
+		.attr("y", width_abs_kozmu02/(900/15))
+        .attr("font-size", width_abs_kozmu02/(900/19))
+		.text("Hátralékos fogyasztók a tartozás hossza szerint");
+    
+	svg_kozmu02_title.append('text')
+		.attr('id', 'kozmu02_title')
+		.attr("x",   (width -   margin_kozmu02.left - margin_kozmu02.right)/2 - margin_kozmu02.left  )
+		.attr("y", width_abs_kozmu02/(900/35))
+        .attr("font-size", width_abs_kozmu02/(900/19))
+		.text("(Millió, 2017. december 31.)");
+}
 
-var z_kozmu03 = d3.scaleOrdinal()
-.range(["#385988", "#43B02A", "#FF671F", "#A4343A"]);
+function MyTitle2(){
+    d3.selectAll("#svg_kozmu02_title > *").remove();
 
-var svg_kozmu03 = d3.select("#viscontainer-kozmu03").append("svg")
-.attr("width", width + margin_kozmu03.left + margin_kozmu03.right)
-.attr("height", 380 + margin_kozmu03.top + margin_kozmu03.bottom)
-.attr("id","svg_kozmu03")
-.append("g")
-.attr("transform", "translate(" + margin_kozmu03.left + "," + margin_kozmu03.top + ")");
+	var margin_kozmu02 = {top: width_abs_kozmu02/(900/20), right: 0, bottom: width_abs_kozmu02/(900/60), left: width_abs_kozmu02/(900/60)},
+		width = width_abs_kozmu02 - margin_kozmu02.left - margin_kozmu02.right,
+		height = 450 - margin_kozmu02.top - margin_kozmu02.bottom;
+
+	var svg_kozmu02_title = d3.select("#svg_kozmu02_title")
+	.attr("width", width + margin_kozmu02.left + margin_kozmu02.right)
+	.attr("height", width_abs_kozmu02/(900/50));
+
+	svg_kozmu02_title.append('text')
+		.attr('id', 'kozmu02_title')
+		.attr("x",  (width -   margin_kozmu02.left - margin_kozmu02.right)/2 - margin_kozmu02.left   )
+		.attr("y", width_abs_kozmu02/(900/15))
+        .attr("font-size", width_abs_kozmu02/(900/19))
+		.text("Hátralékos fogyasztók a tartozás hossza szerint");
+    
+	svg_kozmu02_title.append('text')
+		.attr('id', 'kozmu02_title')
+		.attr("x",   (width -   margin_kozmu02.left - margin_kozmu02.right)/2 - margin_kozmu02.left  )
+		.attr("y", width_abs_kozmu02/(900/35))
+        .attr("font-size", width_abs_kozmu02/(900/19))
+		.text("(%, 2017. december 31.)");
+}
+
+function stacked_chart(){
+	d3.select("#svg_kozmu02").remove();
+
+	var margin_kozmu02 = {top: width_abs_kozmu02/(900/20), right: 0, bottom: width_abs_kozmu02/(900/60), left: width_abs_kozmu02/(900/60)},
+		width = width_abs_kozmu02 - margin_kozmu02.left - margin_kozmu02.right,
+		height = 450 - margin_kozmu02.top - margin_kozmu02.bottom;
+
+	var x_kozmu02 = d3.scaleBand()
+	.rangeRound([0, width - width_abs_kozmu02/(900/300)])
+	.paddingInner(0.15)
+	.align(0.8);
+
+	var y_kozmu02 = d3.scaleLinear()
+	.rangeRound([380, 0]);
+
+	var z_kozmu02 = d3.scaleOrdinal()
+	.range(["#385988", "#43B02A", "#FF671F", "#A4343A"]);
+
+	var svg_kozmu02 = d3.select("#viscontainer-kozmu02").append("svg")
+	.attr("width", width + margin_kozmu02.left + margin_kozmu02.right)
+	.attr("height", 380 + margin_kozmu02.top + margin_kozmu02.bottom)
+	.attr("id","svg_kozmu02")
+	.append("g")
+	.attr("transform", "translate(" + margin_kozmu02.left + "," + margin_kozmu02.top + ")");
 
 
-d3.csv("../../data/04_eladosodas/04_kozmu03.csv", function(d, i, columns) {
-	for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-	d.total = t;
-	return d;
-}, function(error, data) {
-	if (error) throw error;
+	d3.csv("../../data/04_eladosodas/04_kozmu02.csv", function(d, i, columns) {
+		for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+		d.total = t;
+		return d;
+	}, function(error, data) {
+		if (error) throw error;
 
-	console.log(data);
-	var keys_kozmu03 = data.columns.slice(1);
+		var keys_kozmu02 = data.columns.slice(1);
 
-	var serie_kozmu03 = svg_kozmu03.selectAll(".serie_kozmu03")
-	.data(d3.stack().keys(keys_kozmu03)(data))
-	.enter().append("g")
-	.attr("class", "serie_kozmu03");
-
-	data.sort(function(a, b) { return b.total - a.total; });
-	x_kozmu03.domain(data.map(function(d) { return d.type; }));
-	y_kozmu03.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-	z_kozmu03.domain(keys_kozmu03);
-
-	serie_kozmu03.append("g")
-		.selectAll("g")
-		.data(d3.stack().keys(keys_kozmu03)(data))
+		var serie_kozmu02 = svg_kozmu02.selectAll(".serie_kozmu02")
+		.data(d3.stack().keys(keys_kozmu02)(data))
 		.enter().append("g")
-		.attr("fill", function(d) { return z_kozmu03(d.key); })
-		.selectAll("rect")
-		.data(function(d) { return d; })
-		.enter().append("rect")
-		.attr("x", function(d) { return x_kozmu03(d.data.type); })
-		.attr("y", function(d) { return y_kozmu03(d[1]); })
-		.attr("height", function(d) { return y_kozmu03(d[0]) - y_kozmu03(d[1]); })
-		.attr("width", x_kozmu03.bandwidth());
+		.attr("class", "serie_kozmu02");
 
-	
+		data.sort(function(a, b) { return b.total - a.total; });
+		x_kozmu02.domain(data.map(function(d) { return d.name; }));
+		y_kozmu02.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
+		z_kozmu02.domain(keys_kozmu02);
 
-svg_kozmu03.append('text')
-		.attr('id', 'title_0304')
-		.attr("x",  (width - width_abs_kozmu03/(820/100) )/2)
-        .attr("font-size", width_abs_kozmu03/(820/19))
-		.attr("y", -width_abs_kozmu03/(820/25))
-		.text("90 napon túli késedelmes hitelszerződések száma (2017. december)");
+		serie_kozmu02.append("g")
+			.selectAll("g")
+			.data(d3.stack().keys(keys_kozmu02)(data))
+			.enter().append("g")
+			.attr("fill", function(d) { return z_kozmu02(d.key); })
+			.selectAll("rect")
+			.data(function(d) { return d; })
+			.enter().append("rect")
+			.attr("x", function(d) { return x_kozmu02(d.data.name); })
+			.attr("y", function(d) { return y_kozmu02(d[1]); })
+			.attr("height", function(d) { return y_kozmu02(d[0]) - y_kozmu02(d[1]); })
+			.attr("width", x_kozmu02.bandwidth());
+
 		
+		svg_kozmu02.append("g")
+			.attr("class", "axis_kozmu02")
+            .attr("font-size", width_abs_kozmu02/(900/15) )
+			.attr("transform", "translate(0," + 380 + ")")
+			.call(d3.axisBottom(x_kozmu02));
+        
+ 		svg_kozmu02.selectAll(".tick")
+            .attr("font-size", width_abs_kozmu02/(900/15) )
+       
 
-	svg_kozmu03.append("g")
-		.attr("class", "axis_kozmu03")
-        .attr("font-size", width_abs_kozmu03/(820/15))    
-		.attr("transform", "translate(0," + 380 + ")")
-		.call(d3.axisBottom(x_kozmu03))
-	    .selectAll(".tick text")
-        .attr("font-size", width_abs_kozmu03/(820/15))        
-		.call(wrap, x_kozmu03.bandwidth());;
-
-
-	svg_kozmu03.append("g")
-		.attr("class", "axis_kozmu03")
-		.call(d3.axisLeft(y_kozmu03).tickFormat(d3.format("d")))
-		.append("text")
-		.attr("x", 2)
-        .attr("font-size", width_abs_kozmu03/(820/15))        
-		.attr("y", y_kozmu03(y_kozmu03.ticks().pop()))
-		.attr("dy", "0.32em")
-        .attr("text-anchor", "start");
-
-    
-    svg_kozmu03.selectAll(".tick")
-        .attr("font-size", width_abs_kozmu03/(820/15))        
-    
-	var legend_kozmu03 = svg_kozmu03.append("g")
-		.attr("text-anchor", "end")
-		.selectAll("g")
-		.data(keys_kozmu03.slice().reverse())
-		.enter().append("g")
-		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-	legend_kozmu03.append("rect")
-		.attr("x", width - width_abs_kozmu03/(820/90))
-		.attr("width", width_abs_kozmu03/(820/18))
-		.attr("height", width_abs_kozmu03/(820/18))
-		.attr("fill", z_kozmu03);
-
-	legend_kozmu03.append("text")
-		.attr("class", "legend_kozmu03")
-		.attr("x", width-width_abs_kozmu03/(820/60))
-		.attr("y", 8)
-        .attr("font-size", width_abs_kozmu03/(820/16))    
-		.attr("dy", "0.32em")
-		.attr("text-anchor", "start")
-		.text(function(d) { return d; });
-
-	var tooltip_kozmu03 = svg_kozmu03.append("g")
-		.attr("class", "tooltip_kozmu03")
-		.style("visibility", "hidden");
-
-	tooltip_kozmu03.append("rect")
-		.attr("width", width_abs_kozmu03/(820/60))
-		.attr("height",width_abs_kozmu03/(820/20))
-		.attr("fill", "white")
-		.attr("stroke", "#666")
-		.attr("stroke-width", "0.5px");
-
-	tooltip_kozmu03.append("text")
-		.attr("x", width_abs_kozmu03/(820/30))
-		.attr("dy", "1.2em")
-        .attr("font-size", width_abs_kozmu03/(820/16))
-        .style("text-anchor", "middle");	
-
-	serie_kozmu03.selectAll("rect")
-		.on("mouseout", function() { tooltip_kozmu03.style("display", "none"); })
-		.on("mousemove", function(d) {
-		var delta_kozmu03 = d[1]-d[0];
-		var xPosition_kozmu03 = d3.mouse(this)[0] - 25;
-		var yPosition_kozmu03 = d3.mouse(this)[1] - 30;
-		tooltip_kozmu03
-			.style("visibility", "visible")
-			.style("display", "inline")
-			.attr("transform", "translate(" + xPosition_kozmu03 + "," + yPosition_kozmu03 + ")")
-			.select("text").text((delta_kozmu03).toFixed(0));
-	});
-
-serie_kozmu03.append('text')
-	.attr("class", "kozmu03_forras")
-		.attr("x", width- width_abs_kozmu03/(820/100))
-		.attr("y", height_kozmu03 + margin_kozmu03.top + margin_kozmu03.bottom*0.99)
-        .attr("font-size", width_abs_kozmu03/(820/15))    
-	.attr("text-anchor", "end")  
-	.text("Adatok forrása: MNB")
-	.on('click', function(d) {
-		window.open(
-			'http://www.mnb.hu/statisztika/statisztikai-adatok-informaciok/adatok-idosorok',
-			'_blank' // <- This is what makes it open in a new window.
-		);
-	})
-	.on('mouseover', function(d){
-		d3.select(this).style("cursor", "pointer"); 
-	})
-
-	.on("mouseout", function() { d3.select(this).style("cursor", "default"); })
-	.on("mousemove", function(d) {
-	d3.select(this).style("cursor", "pointer"); 
-	});
-	
-	
+var locale = d3.formatLocale({
+  decimal: ",",
 });
 
 
-function wrap(text, width) {
-  text.each(function() {
-    var text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.1, // ems
-        y = text.attr("y"),
-        dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em")
-    while (word = words.pop()) {
-      line.push(word)
-      tspan.text(line.join(" "))
-      if (tspan.node().getComputedTextLength() > width) {
-        line.pop()
-        tspan.text(line.join(" "))
-        line = [word]
-        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", `${++lineNumber * lineHeight + dy}em`).text(word)
-      }
-    }
-  })
+		
+var format = locale.format(",.1f");
+		
+		svg_kozmu02.append("g")
+			.attr("class", "axis_kozmu02")
+			.call(d3.axisLeft(y_kozmu02).tickFormat(function(d){
+ if(this.parentNode.nextSibling){
+return format(d/1000000) + " M"} else { return format(d/1000000)  + " M"}}))
+			.append("text")
+			.attr("x", 2)
+			.attr("y", y_kozmu02(y_kozmu02.ticks().pop()))
+			.attr("dy", "0.32em")
+			.attr("text-anchor", "start");
+
+ 		svg_kozmu02.selectAll(".tick")
+            .attr("font-size", width_abs_kozmu02/(900/16) )        
+        
+		var legend_kozmu02 = svg_kozmu02.append("g")
+			.selectAll("g")
+			.data(keys_kozmu02.slice().reverse())
+			.enter().append("g")
+			.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+		legend_kozmu02.append("rect")
+			.attr("x", width - width_abs_kozmu02/(900/280))
+			.attr("width", width_abs_kozmu02/(900/18))
+			.attr("height", width_abs_kozmu02/(900/18))
+			.attr("fill", z_kozmu02);
+
+		legend_kozmu02.append("text")
+			.attr("class", "legend_kozmu02b")
+			.attr("x", width-width_abs_kozmu02/(900/245))
+			.attr("y", 8)
+			.attr("dy", "0.32em")
+			.attr("text-anchor", "start")
+            .attr("font-size", width_abs_kozmu02/(900/16) )        
+			.text(function(d) { return d; });
+
+		var tooltip_kozmu02 = svg_kozmu02.append("g")
+			.attr("class", "tooltip_kozmu02")
+			.style("visibility", "hidden");
+
+		tooltip_kozmu02.append("rect")
+			.attr("width", width_abs_kozmu02/(900/60))
+			.attr("height", width_abs_kozmu02/(900/25))
+			.attr("fill", "white")
+			.attr("stroke", "#666")
+			.attr("stroke-width", "0.5px");
+
+		tooltip_kozmu02.append("text")
+			.attr("x", width_abs_kozmu02/(900/30))
+			.attr("dy", "1.2em")
+            .attr("font-size", width_abs_kozmu02/(900/15) )            
+			.style("text-anchor", "middle");	
+
+		serie_kozmu02.selectAll("rect")
+			.on("mouseout", function() { tooltip_kozmu02.style("display", "none"); })
+			.on("mousemove", function(d) {
+			var delta_kozmu02 = d[1]-d[0];
+			var xPosition_kozmu02 = d3.mouse(this)[0] - 25;
+			var yPosition_kozmu02 = d3.mouse(this)[1] - 30;
+			tooltip_kozmu02
+				.style("visibility", "visible")
+				.style("display", "inline")
+				.attr("transform", "translate(" + xPosition_kozmu02 + "," + yPosition_kozmu02 + ")")
+				.select("text").text((delta_kozmu02).toFixed(0));
+		});
+
+		serie_kozmu02.append('text')
+			.attr("id", "kozmu02_forras")
+			.attr("x", width- width_abs_kozmu02/(900/300))
+			.attr("y",  380 + margin_kozmu02.bottom*0.66)
+			.attr("text-anchor", "end")
+            .attr("font-size", width_abs_kozmu02/(900/15) )        
+			.text("Adatok forrása: MEKH adatszolgáltatás, 2018.");
+	});
+
+}
+
+
+function normalized_chart(){		
+	d3.select("#svg_kozmu02").remove();
+
+	var margin_kozmu02 = {top: width_abs_kozmu02/(900/20), right: 0, bottom: width_abs_kozmu02/(900/60), left: width_abs_kozmu02/(900/60)},
+		width = width_abs_kozmu02 - margin_kozmu02.left - margin_kozmu02.right,
+		height = 450 - margin_kozmu02.top - margin_kozmu02.bottom;
+
+	var x_kozmu02 = d3.scaleBand()
+		.rangeRound([0, width - width_abs_kozmu02/(900/300)])
+		.paddingInner(0.15)
+		.align(0.8);
+
+	var y_kozmu02 = d3.scaleLinear()
+		.rangeRound([380, 0]);
+
+	var z_kozmu02 = d3.scaleOrdinal()
+		.range(["#385988", "#43B02A", "#FF671F", "#A4343A"]);
+
+	var svg_kozmu02 = d3.select("#viscontainer-kozmu02").append("svg")
+		.attr("width", width + margin_kozmu02.left + margin_kozmu02.right)
+		.attr("height", 380 + margin_kozmu02.top + margin_kozmu02.bottom)
+		.attr("id","svg_kozmu02")
+		.append("g")
+		.attr("transform", "translate(" + margin_kozmu02.left + "," + margin_kozmu02.top + ")");
+
+	var stack = d3.stack()
+		.offset(d3.stackOffsetExpand);
+
+	d3.csv("../../data/04_eladosodas/04_kozmu02.csv", type, function(error, data) {
+		if (error) throw error;
+
+		data.sort(function(a, b) { return b.total - a.total; });
+
+		x_kozmu02.domain(data.map(function(d) { return d.name; }));
+		z_kozmu02.domain(data.columns.slice(1));
+
+		var serie_kozmu02 = svg_kozmu02.selectAll(".serie_kozmu02")
+			.data(stack.keys(data.columns.slice(1))(data))
+			.enter().append("g")
+			.attr("class", "serie_kozmu02")
+			.attr("fill", function(d) { return z_kozmu02(d.key); });
+
+		serie_kozmu02.selectAll("rect")
+			.data(function(d) { return d; })
+			.enter().append("rect")
+			.attr("x", function(d) { return x_kozmu02(d.data.name); })
+			.attr("y", function(d) { return y_kozmu02(d[1]); })
+			.attr("height", function(d) { return y_kozmu02(d[0]) - y_kozmu02(d[1]); })
+			.attr("width", x_kozmu02.bandwidth());
+
+		svg_kozmu02.append("g")
+			.attr("class", "axis_kozmu02")
+            .attr("font-size", width_abs_kozmu02/(900/16) )        
+			.attr("transform", "translate(0," + 380 + ")")
+			.call(d3.axisBottom(x_kozmu02));
+
+		svg_kozmu02.append("g")
+			.attr("class", "axis_kozmu02")
+			.call(d3.axisLeft(y_kozmu02).ticks(10, "%"));
+
+ 		svg_kozmu02.selectAll(".tick")
+            .attr("font-size", width_abs_kozmu02/(900/16) )    
+        
+		var legendClassArray_kozmu02 = []; 
+		var legendClassArray_orig_kozmu02 = []; 
+
+		var legend_kozmu02 = svg_kozmu02.selectAll(".legend_kozmu02")
+		.data(z_kozmu02.domain().slice().reverse())
+		.enter().append("g")
+		.attr("class", function (d) {
+			legendClassArray_kozmu02.push(d.replace(/\s+|[,]+\s/g, '')); 
+			legendClassArray_orig_kozmu02.push(d); 
+			return "legend_kozmu02";
+		})
+		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+		legend_kozmu02.append("rect")
+			.attr("x", width - width_abs_kozmu02/(900/280))
+			.attr("width", width_abs_kozmu02/(900/18))
+			.attr("height", width_abs_kozmu02/(900/18))
+			.style("fill", z_kozmu02)
+			.attr("id", function (d, i) {
+			return "id" + d.replace(/\s+|[,]+\s/g, '')});	
+
+		legend_kozmu02.append("text")
+			.attr("class", "legend_kozmu02")
+			.attr("x", width-width_abs_kozmu02/(900/245))
+			.attr("y", 8)
+			.attr("dy", "0.32em")
+			.attr("text-anchor", "start")
+            .attr("font-size", width_abs_kozmu02/(900/16) )    
+			.text(function(d) { return d; });
+
+		var tooltip_kozmu02 = svg_kozmu02.append("g")
+			.attr("class", "tooltip_kozmu02")
+			.style("display", "none");
+
+		tooltip_kozmu02.append("rect")
+			.attr("width", width_abs_kozmu02/(900/60))
+			.attr("height", width_abs_kozmu02/(900/25))
+			.attr("fill", "white")
+			.attr("stroke", "#666")
+			.attr("stroke-width", "0.5px");
+
+		tooltip_kozmu02.append("text")
+			.attr("x", width_abs_kozmu02/(900/30))
+			.attr("dy", "1.2em")
+            .attr("font-size", width_abs_kozmu02/(900/15) )                    
+			.style("text-anchor", "middle");	
+
+		serie_kozmu02.selectAll("rect")
+			.on("mouseover", function() { tooltip_kozmu02.style("display", null); })
+			.on("mouseout", function() { tooltip_kozmu02.style("display", "none"); })
+			.on("mousemove", function(d) {
+			var delta_kozmu02 = d[1]-d[0];
+			var xPosition_kozmu02 = d3.mouse(this)[0] - 25;
+			var yPosition_kozmu02 = d3.mouse(this)[1] - 30;
+			tooltip_kozmu02.attr("transform", "translate(" + xPosition_kozmu02 + "," + yPosition_kozmu02 + ")")
+				.select("text").text(((delta_kozmu02*100).toFixed(1)).toString().replace(".",",") +"%");
+		});
+
+		serie_kozmu02.append('text')
+			.attr("id", "kozmu02_forras")
+			.attr("x", width- width_abs_kozmu02/(900/300))
+			.attr("y",  380 + margin_kozmu02.bottom*0.66)
+            .attr("font-size", width_abs_kozmu02/(900/15) )                
+			.text("Adatok forrása: MEKH adatszolgáltatás, 2018.");
+
+	});
+
+
+	function type(d, i, columns) {
+		for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+		d.total = t;
+		return d;
+	}
 }
--- js/04_kozmu03.js
+++ js/04_kozmu03.js
@@ -10,17 +10,19 @@ if (d3.select("#viscontainer-kozmu03").node().getBoundingClientRect().width  > 6
 };
 
 
-var margin_kozmu03 = {top: width_abs_kozmu03/(820/40), right: 0, bottom: width_abs_kozmu03/(820/80), left: width_abs_kozmu03/(820/60)},
+var margin_kozmu03 = {top: width_abs_kozmu03/(820/40), right: 0, bottom: width_abs_kozmu03/(820/100), left: width_abs_kozmu03/(820/70)},
 	width = width_abs_kozmu03 - margin_kozmu03.left - margin_kozmu03.right,
 	height_kozmu03 = 450 - margin_kozmu03.top - margin_kozmu03.bottom;
 
 var x_kozmu03 = d3.scaleBand()
-.rangeRound([0, width - width_abs_kozmu03/(820/150)])
+.rangeRound([0, width - width_abs_kozmu03/(820/100)])
 .paddingInner(0.10)
 .align(0.8);
 
 var y_kozmu03 = d3.scaleLinear()
-.rangeRound([380, 0]);
+.rangeRound([380, 0])
+
+;
 
 var z_kozmu03 = d3.scaleOrdinal()
 .range(["#385988", "#43B02A", "#FF671F", "#A4343A"]);
@@ -70,34 +72,35 @@ d3.csv("../../data/04_eladosodas/04_kozmu03.csv", function(d, i, columns) {
 
 svg_kozmu03.append('text')
 		.attr('id', 'title_0304')
-		.attr("x",  (width - width_abs_kozmu03/(820/150) )/2)
-        .attr("font-size", width_abs_kozmu03/(820/17))
+		.attr("x",  (width - width_abs_kozmu03/(820/100) )/2)
+        .attr("font-size", width_abs_kozmu03/(820/19))
 		.attr("y", -width_abs_kozmu03/(820/25))
 		.text("90 napon túli késedelmes hitelszerződések száma (2017. december)");
 		
 
 	svg_kozmu03.append("g")
 		.attr("class", "axis_kozmu03")
-        .attr("font-size", width_abs_kozmu03/(820/13))    
+        .attr("font-size", width_abs_kozmu03/(820/15))    
 		.attr("transform", "translate(0," + 380 + ")")
 		.call(d3.axisBottom(x_kozmu03))
 	    .selectAll(".tick text")
+        .attr("font-size", width_abs_kozmu03/(820/15))        
 		.call(wrap, x_kozmu03.bandwidth());;
 
 
 	svg_kozmu03.append("g")
 		.attr("class", "axis_kozmu03")
-		.call(d3.axisLeft(y_kozmu03))
+		.call(d3.axisLeft(y_kozmu03).tickFormat(d3.format("d")))
 		.append("text")
 		.attr("x", 2)
-        .attr("font-size", width_abs_kozmu03/(820/13))        
+        .attr("font-size", width_abs_kozmu03/(820/15))        
 		.attr("y", y_kozmu03(y_kozmu03.ticks().pop()))
 		.attr("dy", "0.32em")
         .attr("text-anchor", "start");
 
     
     svg_kozmu03.selectAll(".tick")
-        .attr("font-size", width_abs_kozmu03/(820/13))        
+        .attr("font-size", width_abs_kozmu03/(820/15))        
     
 	var legend_kozmu03 = svg_kozmu03.append("g")
 		.attr("text-anchor", "end")
@@ -116,7 +119,7 @@ svg_kozmu03.append('text')
 		.attr("class", "legend_kozmu03")
 		.attr("x", width-width_abs_kozmu03/(820/60))
 		.attr("y", 8)
-        .attr("font-size", width_abs_kozmu03/(820/14))    
+        .attr("font-size", width_abs_kozmu03/(820/16))    
 		.attr("dy", "0.32em")
 		.attr("text-anchor", "start")
 		.text(function(d) { return d; });
@@ -127,7 +130,7 @@ svg_kozmu03.append('text')
 
 	tooltip_kozmu03.append("rect")
 		.attr("width", width_abs_kozmu03/(820/60))
-		.attr("height",width_abs_kozmu03/(820/20))
+		.attr("height",width_abs_kozmu03/(820/25))
 		.attr("fill", "white")
 		.attr("stroke", "#666")
 		.attr("stroke-width", "0.5px");
@@ -135,7 +138,7 @@ svg_kozmu03.append('text')
 	tooltip_kozmu03.append("text")
 		.attr("x", width_abs_kozmu03/(820/30))
 		.attr("dy", "1.2em")
-        .attr("font-size", width_abs_kozmu03/(820/14))
+        .attr("font-size", width_abs_kozmu03/(820/16))
         .style("text-anchor", "middle");	
 
 	serie_kozmu03.selectAll("rect")
@@ -153,9 +156,9 @@ svg_kozmu03.append('text')
 
 serie_kozmu03.append('text')
 	.attr("class", "kozmu03_forras")
-		.attr("x", width- width_abs_kozmu03/(820/150))
-		.attr("y", height_kozmu03 + margin_kozmu03.top + margin_kozmu03.bottom*0.8)
-        .attr("font-size", width_abs_kozmu03/(820/13))    
+		.attr("x", width- width_abs_kozmu03/(820/100))
+		.attr("y", height_kozmu03 + margin_kozmu03.top + margin_kozmu03.bottom*0.99)
+        .attr("font-size", width_abs_kozmu03/(820/15))    
 	.attr("text-anchor", "end")  
 	.text("Adatok forrása: MNB")
 	.on('click', function(d) {
